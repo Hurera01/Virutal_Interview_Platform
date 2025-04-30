abcab55ae271127f5fc5777f889545513878e443
06B-121\Hurera1011 <hurera1011@hulhub.com>
2025-04-30 18:41:34 +0500

    add room in hub

--- Virtual Interview Platform/VideoHub/InterviewHub.cs
+++ Virtual Interview Platform/VideoHub/InterviewHub.cs
@@ -1,30 +1,37 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using Virtual_Interview_Platform.DTO.RTCDto;
 
 namespace Virtual_Interview_Platform.VideoHub
 {
     public sealed class InterviewHub : Hub
     {
+        public async Task JoinRoom(string roomId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            //await Clients.Group(roomId).SendAsync("ReceiveMessage", $"{Context.ConnectionId} joined room {roomId}");
+        }
+
         public override async Task OnConnectedAsync()
         {
             Console.WriteLine("--------");
             await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} has joined");
         }
 
-        public async Task SendOffer(RtcOfferDto offer)
+        public async Task SendOffer(string roomId,RtcOfferDto offer)
          {
             Console.WriteLine("--------");
-            await Clients.Others.SendAsync("Receive Offer", offer);
+            await Clients.OthersInGroup(roomId).SendAsync("Receive Offer", offer);
         }
 
-        public async Task SendAnswer(RtcAnswerDto answer)
+        public async Task SendAnswer(string roomId, RtcAnswerDto answer)
         {
-            await Clients.Others.SendAsync("Receive Answer", answer);
+            await Clients.OthersInGroup(roomId).SendAsync("Receive Answer", answer);
         }
 
-        public async Task SendIceCandidate(IceCandidateDto candidate)
+        public async Task SendIceCandidate(string roomId, IceCandidateDto candidate)
         {
-            await Clients.Others.SendAsync("Receive IceCandidate", candidate);
+            await Clients.OthersInGroup(roomId).SendAsync("Receive IceCandidate", candidate);
         }
     }
 }
